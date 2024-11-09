b675aacd79b2f5c811e6dee92a1d54124c0955e4
Amaan Wanie <130792177+AmaanWanie@users.noreply.github.com>
2024-11-10 00:19:25 +0530

    easy

---
+++ DailyQuestions/1331. Rank Transform of an Array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        cop=arr.copy()
+        cop.sort()
+        HM = dict()
+        new = []
+        rank= 1
+        for i in range(len(cop)):
+            if cop[i] not in HM:
+                HM[cop[i]] = rank
+                count+=1
+            
+        
+
+        for i in range(len(arr)):
+            new.append(HM[arr[i]])
+            
+
+        return new
